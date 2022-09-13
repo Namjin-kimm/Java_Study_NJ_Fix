251c593ecf57152dac3a542ccffc2cd1eb26d30b
Namjin-kimm <namjin-kim@daum.net>
2022-09-13 20:00:55 +0900

    error, interceptor

---
+++ src/main/java/com/iu/start/error/BackErrorController.java
@@ -0,0 +1,26 @@
+package com.iu.start.error;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/errors/*")
+public class BackErrorController {
+
+	@GetMapping("error404")
+	public ModelAndView error404()throws Exception{
+		ModelAndView mv = new ModelAndView();
+		System.out.println("404");
+		mv.setViewName("/errors/error_404");
+		return mv;
+	}
+	@GetMapping("error400")
+	public ModelAndView error400()throws Exception{
+		ModelAndView mv = new ModelAndView();
+		System.out.println("400");
+		mv.setViewName("/errors/error_404");
+		return mv;
+	}
+}
